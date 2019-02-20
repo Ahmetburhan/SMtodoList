fc5ce4d0c15b55a756a87bc72d150cd861a36b65
Ahmet Koylan <Ahmetburhan@gmail.com>
2019-02-19 21:09:57 -0800

    added some style

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import TodoList from "./components/todolist";
+import TodoList from "./components/toDoList";
 
 import './App.css';
 
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>Hello!</p>
+        <p>Stanford Tasklist</p>
         <TodoList />
 
       </div>
